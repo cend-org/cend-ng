0715316dd0d7c22b83d497367b08fa2f7fe9c8a0
Julis <julis@thegreenshot.green>
2024-05-09 10:56:19 +0200

    creating run.sh to avoid system dup and service run outload in ubuntu aws agent

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Updating code from Git..."
-cd /usr/workspace/cend-ng && sudo git fetch origin && sudo git reset --hard origin/master
+cd /usr/workspace/cend-ng && sudo git fetch origin && sudo git reset --hard origin/main
 
 echo "running..."
 sudo npm run serve:ssr:angular-app
